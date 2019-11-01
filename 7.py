1b8eaa2c4930e2e3676643e33142a8fcde455d32
Tina Mishra <54836357+tina5mishra@users.noreply.github.com>
2019-11-01 15:13:58 +0530

    7.py
    addition subtraction multiplication and division using python programming.

---
+++ 7.py
@@ -0,0 +1,27 @@
+def add():
+	x = int(input("enter a value of x\n"))
+	y = int(input("enetr a value of y\n"))
+	sum = x+y
+	print(sum) 
+add()
+
+def sub():
+	x = int(input("enter a value of x\n"))
+	y = int(input("enetr a value of y\n"))
+	sub = x-y
+	print(sub) 
+sub()
+
+def mul():
+	x = int(input("enter a value of x\n"))
+	y = int(input("enetr a value of y\n"))
+	mul = x*y
+	print(mul) 
+mul()
+
+def div():
+	x = int(input("enter a value of x\n"))
+	y = int(input("enetr a value of y\n"))
+	div = x/y
+	print(div) 
+div()
