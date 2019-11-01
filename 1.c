400ffa5974d7ccbc5cdc344fa513ed62ea9f384d
Tina Mishra <54836357+tina5mishra@users.noreply.github.com>
2019-11-01 14:03:08 +0530

    Add files via upload
    C prog.

---
+++ 1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10;
+	int b=10;
+	
+	if(a>b)
+	{
+		printf("A");
+	}
+	else if(a<b)
+	{
+		printf("B");
+	}	
+	else(a=b);
+	{
+		printf("C");
+	}
+	getch();
+}
