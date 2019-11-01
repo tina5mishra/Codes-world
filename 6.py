23f4e464cf3d6399c80450d4e813b04f10bd3061
Tina Mishra <54836357+tina5mishra@users.noreply.github.com>
2019-11-01 15:11:09 +0530

    6.py
    creating dictionary using print tuple ,set,list .

---
+++ 6.py
@@ -0,0 +1,18 @@
+d2 = {}
+d2[10] = 'adminstration'
+d2[20] = 'marketing'
+d2[30] = 'purchasing'
+d2[40] = 'infomation technology'
+print(d2)
+print(list(d2.keys()))
+print(list(d2.values()))
+print(list(d2.items()))
+print(tuple(d2.keys()))
+print(tuple(d2.values()))
+print(tuple(d2.items()))
+print(set(d2.keys()))
+print(set(d2.values()))
+print(set(d2.items()))
+
+
+
